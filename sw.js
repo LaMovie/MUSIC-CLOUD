40005435deeec2455d4c698f612ac21bb6480f0d
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-07-27 21:13:08 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('music-cloud-cache-v1').then((cache) => {
+    caches.open('music-cloud-cache-v2').then((cache) => {
       return cache.addAll([
         './index.html',
         './Styles.css',
